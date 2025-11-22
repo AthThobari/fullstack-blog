c1c3987754d70310e946e0d3d066db0213e07a09
AthThobari <thobari.mutu@gmail.com>
2025-11-23 05:50:20 +0700

    chore: stop tracking .env

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import ImageKit from "./Image";
+import { Link } from "react-router";
 
 function Navbar() {
     const [open, setOpen] = useState(false);
@@ -7,34 +8,34 @@ function Navbar() {
   return (
     <div className="w-full h-16 md:h-20 flex items-center justify-between">
         {/* LOGO */}
-        <div className="flex items-center gap-4 text-2xl font-bold">
+        <Link to={"/"} className="flex items-center gap-4 text-2xl font-bold">
             <ImageKit src="/logo.png" alt="Lama Logo" w={32} h={32}/>
             <span>lamalog</span>
-        </div>
+        </Link>
         {/* MOBILE MENU */}
         <div className="md:hidden">
             <div className="cursor-pointer text-4xl" onClick={() => setOpen((prev) => !prev)}>{open ? "X" : "≡"}</div>
 
             {/* Mobile link list */}
             <div className={`w-full h-screen flex flex-col items-center gap-8 font-medium text-lg justify-center absolute top-16 bg-color-[#e6e6ff] transition-all ease-in-out ${open ? "right-0" : "-right-full"}`}>
-                <a href="/">Home</a>
-            <a href="/">Trending</a>
-            <a href="/">Most Popular</a>
-            <a href="/">About</a>
-            <a href="">
+                <Link to={"/"}>Home</Link>
+            <Link to={"/"}>Trending</Link>
+            <Link to={"/"}>Most Popular</Link>
+            <Link to={"/"}>About</Link>
+            <Link to={"/"}>
                 <button className="bg-blue-800 text-white py-2 px-4 rounded-3xl">Login 👋</button>
-            </a>
+            </Link>
             </div>
         </div>
         {/* DEKSTOP MENU */}
         <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
-            <a href="/">Home</a>
-            <a href="/">Trending</a>
-            <a href="/">Most Popular</a>
-            <a href="/">About</a>
-            <a href="">
+            <Link to={"/"}>Home</Link>
+            <Link to={"/"}>Trending</Link>
+            <Link to={"/"}>Most Popular</Link>
+            <Link to={"/"}>About</Link>
+            <Link to={"/"}>
                 <button className="bg-blue-800 text-white py-2 px-4 rounded-3xl">Login 👋</button>
-            </a>
+            </Link>
         </div>
     </div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -8,15 +8,21 @@ import Write from "./routes/Write";
 import LoginPage from "./routes/LoginPage";
 import RegisterPage from "./routes/RegisterPage";
 import SinglePostPage from './routes/SinglePostPage';
+import MainLayout from './layouts/MainLayout';
 
 
 const router = createBrowserRouter([
-  { path: "/", element: <Homepage/> },
+  {
+    element: <MainLayout/>,
+    children: [
+      { path: "/", element: <Homepage/> },
   { path: "/posts", element: <PostListPage/> },
   { path: "/:slug", element: <SinglePostPage/> },
   { path: "/write", element: <Write /> },
   { path: "/login", element: <LoginPage/> },
   { path: "/register", element: <RegisterPage/> },
+    ]
+  }
 ]);
 
 
