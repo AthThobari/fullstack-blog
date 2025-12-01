da3999424bc98f1fa548933f17cd1512b318cd4d
AthThobari <thobari.mutu@gmail.com>
2025-12-01 10:27:51 +0700

    feat(route): change default route

--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -5,24 +5,37 @@ function PostListItem() {
   return (
     <div className="flex flex-col xl:flex-row gap-8">
       <div className="md:hidden xl:block xl:w-1/3">
-        <ImageKit src="postImg.jpeg" className="rounded-2xl object-cover" w={734}/>
+        <ImageKit
+          src="postImg.jpeg"
+          className="rounded-2xl object-cover"
+          w={734}
+        />
       </div>
       {/* details */}
       <div className="flex flex-col gap-4 xl:w-2/3">
-        <Link to={"/test"} className="text-4xl font-semibold">
+        <Link to={"/blog/test"} className="text-4xl font-semibold">
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </Link>
         <div className="flex items-center gap-2 text-gray-400 text-sm">
-            <span>Written by</span>
-            <Link to={""} className="text-blue-800">John Doe</Link>
-            <span>on</span>
-            <Link to={""} className="text-blue-800">Web Design</Link>
-            <span>2 days ago</span>
+          <span>Written by</span>
+          <Link to={""} className="text-blue-800">
+            John Doe
+          </Link>
+          <span>on</span>
+          <Link to={""} className="text-blue-800">
+            Web Design
+          </Link>
+          <span>2 days ago</span>
         </div>
         <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem necessitatibus odit voluptatum aliquam optio excepturi repellendus, officiis quia atque velit quidem, eveniet unde eos omnis id molestiae eius ad tenetur!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+          necessitatibus odit voluptatum aliquam optio excepturi repellendus,
+          officiis quia atque velit quidem, eveniet unde eos omnis id molestiae
+          eius ad tenetur!
         </p>
-        <Link to={"/test"} className="underline text-blue-800 text-sm">Read More</Link>
+        <Link to={"/blog/test"} className="underline text-blue-800 text-sm">
+          Read More
+        </Link>
       </div>
     </div>
   );
